998af4247e4cbeb60caaefae87a23ea2683ff29b
jstortoise <web.dev429@gmail.com>
2022-08-02 09:07:37 -0400

    add seeder for the db init

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +17,28 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Vehicle::factory(10)->create();
+
+        \App\Models\Key::factory(10)->create();
+
+        \App\Models\Technician::factory(10)->create();
+
+        for ($i = 0; $i < 50; $i++)
+        {
+            $key_id = random_int(1, 10);
+            $vehicle_id = random_int(1, 10);
+
+            $duplicate = DB::table('key_vehicle')
+                ->where('key_id', $key_id)
+                ->where('vehicle_id', $vehicle_id)
+                ->first();
+
+            if (!$duplicate) {
+                DB::table('key_vehicle')->insert([
+                    'key_id' => $key_id,
+                    'vehicle_id' => $vehicle_id,
+                ]);
+            }
+        } 
     }
 }
